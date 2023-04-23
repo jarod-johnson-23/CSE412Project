fdae19e52e718589e949f10cf0d752bb457726db
edyssacervantes <edyssanc@cox.net>
2023-04-22 17:16:09 -0700

    Added scroll feature for friends section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [albums, set_albums] = useState([]);
   const [albumName, set_albumName] = useState("");
   const [topUsers, set_topUsers] = useState([]);
-  //const [userName, set_friendship] = useState(""); *EC added
+  const [userName, set_friendship] = useState([]); //EC added
 
   //Base URL of the URL that holds all the APIs, just add the URI to complete the URL ex: /get-user
   let baseURL = "https://cse412project-server.onrender.com";
@@ -117,10 +117,25 @@ function Home() {
     });
   };*/
 
+  const getFriends = async (uid) => {
+    const date = Date.now();
+    let currentDate = null;
+    //Very bad programming practice but makes sure the userInfo cookie is set before using its value in the API call
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < 500);
+    Axios.post(baseURL + "/get-friends", {
+      uid: cookies.userInfo.UID,
+    }).then((response) => {
+      set_friendship(response.data);
+    });
+  };
+
   useEffect(() => {
     window.scrollTo(0, 0);
     getAlbums();
     getTopUsers();
+    getFriends();
   }, []);
 
   return (
@@ -159,7 +174,7 @@ function Home() {
 
       <div className="profile-body">
         <div class="topnav">
-          <a href="#addfriend">Add Friends</a>
+          <a href="#addfriend">Search UserID</a>
           <input
             type="text"
             placeholder="Search.."
@@ -173,6 +188,25 @@ function Home() {
         </div>
         <div className="section friends-div">
           <h2>Friend List</h2>
+          <div class="scroll">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum. It is a long
+            established fact that a reader will be distracted by the readable
+            content of a page when looking at its layout. The point of using
+            Lorem Ipsum is that it has a more-or-less normal distribution of
+            letters, as opposed to using 'Content here, content here', making it
+            look like readable English. Many desktop publishing packages and web
+            page editors now use Lorem Ipsum as their default model text, and a
+            search for 'lorem ipsum' will uncover many web sites still in their
+            infancy.
+          </div>
         </div>
         <div className="section suggestion-div">
           <h2>Suggested Friends</h2>
