01ced147011d6c1baf9daa172c861d15390c5636
edyssacervantes <edyssanc@cox.net>
2023-04-22 19:29:39 -0700

    Update Home.js
    added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -124,11 +124,11 @@ function Home() {
     do {
       currentDate = Date.now();
     } while (currentDate - date < 500);
-    Axios.post(baseURL + "/get-friends", {
-      uid: cookies.userInfo.UID,
-    }).then((response) => {
-      set_friendship(response.data);
-    });
+    Axios.get(baseURL + "/get-friends/:uid" + cookies.userInfo.UID).then(
+      (response) => {
+        set_friendship(response.data);
+      }
+    );
   };
 
   useEffect(() => {
@@ -189,23 +189,9 @@ function Home() {
         <div className="section friends-div">
           <h2>Friend List</h2>
           <div class="scroll">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum. It is a long
-            established fact that a reader will be distracted by the readable
-            content of a page when looking at its layout. The point of using
-            Lorem Ipsum is that it has a more-or-less normal distribution of
-            letters, as opposed to using 'Content here, content here', making it
-            look like readable English. Many desktop publishing packages and web
-            page editors now use Lorem Ipsum as their default model text, and a
-            search for 'lorem ipsum' will uncover many web sites still in their
-            infancy.
+            {userName.map((val, key) => {
+              return <p>{val.fName + " " + val.lName}</p>;
+            })}
           </div>
         </div>
         <div className="section suggestion-div">
